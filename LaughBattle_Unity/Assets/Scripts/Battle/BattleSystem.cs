04c1b8219c60d6756693ca865d29fbd33f55e4f8
shotohayakawa <hayahaya924@gmail.com>
2024-01-28 12:57:06 +0900

    update BattleSystem

--- LaughBattle_Unity/Assets/Scripts/Battle/BattleSystem.cs
+++ LaughBattle_Unity/Assets/Scripts/Battle/BattleSystem.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class BattleSystem : MonoBehaviour
 {
-    public static BattleSystem instance;
+	public static BattleSystem instance;
+    public BulletGenerator bulletGenerator;
     public AudioRecorder_v1 recorder_player_1, recorder_player_2;
 
     public float Player_1_HP, Player_2_HP;
-    float vol_player_1, freq_player_1, vol_player_2, freq_player_2;
+    public float vol_player_1, freq_player_1, vol_player_2, freq_player_2;
     bool enableShot_p1 = true, enableShot_p2 = true;
     void Awake()
     {
@@ -38,13 +39,13 @@ public class BattleSystem : MonoBehaviour
 
         if(enableShot_p1 == true)
         {
-            if (vol_player_1 > 0.1f)
+            if (vol_player_1 > 0.2f)
             {
-                //発射
-                enableShot_p1 = false;
-            }
-
-        }
+                bulletGenerator._emit_bullet_player_1(freq_player_1);
+				enableShot_p1 = false;
+                Debug.Log(freq_player_1);
+			}
+		}
         else
         {
             if (vol_player_1 < 0.01f)
@@ -52,26 +53,41 @@ public class BattleSystem : MonoBehaviour
                 enableShot_p1 = true;
 
             }
+		}
+	}
 
+	void runningBulletShooting_player_2()
+	{
+		vol_player_2 = recorder_player_2.volume;
+		freq_player_2 = recorder_player_2.freq;
+
+        if (enableShot_p2 == true)
+        {
+            if (vol_player_2 > 0.2f)
+            {
+                bulletGenerator._emit_bullet_player_2(freq_player_2);
+				enableShot_p2 = false;
+                Debug.Log(freq_player_2);
+            }
+		}
+        else
+        {
+            if (vol_player_2 < 0.01f)
+            {
+                enableShot_p2 = true;
+
+            }
         }
     }
 
-    void runningBulletShooting_player_2()
+	public void _playAudio_Player_1()
     {
-        vol_player_2 = recorder_player_2.volume;
-        freq_player_2 = recorder_player_2.freq;
-    }
-
-    public void _playAudio_Player_1()
-    {
-
-
-
+        recorder_player_1.PlayRecording();
     }
 
     public void _playAudio_Player_2()
     {
-
+        recorder_player_2.PlayRecording();
     }
 
     public void _Start_Battle()
