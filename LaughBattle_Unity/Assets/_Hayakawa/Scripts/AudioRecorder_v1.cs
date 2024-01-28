c28cb993da3488f1bcb27fd01ffc15501e69350b
shotohayakawa <hayahaya924@gmail.com>
2024-01-28 14:13:54 +0900

    update BulletGenerator

--- LaughBattle_Unity/Assets/_Hayakawa/Scripts/AudioRecorder_v1.cs
+++ LaughBattle_Unity/Assets/_Hayakawa/Scripts/AudioRecorder_v1.cs
@@ -108,7 +108,7 @@ public class AudioRecorder_v1 : MonoBehaviour
 
 		var ___freq = (float)maxIndex * AudioSettings.outputSampleRate / 2 / spectrum.Length;
 
-		var __freq = ___freq.Map(200f, 800f, 0f, 1f);
+		var __freq = ___freq.Map(500f, 1500f, 0f, 1f);
 		_freq = Mathf.Lerp(_freq, __freq, lerpRate);
 		freq = Mathf.Clamp(_freq, 0f, 1f);
 	}
